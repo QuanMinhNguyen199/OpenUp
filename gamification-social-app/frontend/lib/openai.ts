790c82f0971517790017bd967759ff695264ca6c
Tris <95870632+coding911@users.noreply.github.com>
2026-04-19 09:12:59 +0700

    fix: remove (#21)

--- gamification-social-app/frontend/lib/openai.ts
+++
@@ -1,7 +0,0 @@
-// tạm thời bỏ file này
-import OpenAI from 'openai';
-
-export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  baseURL: 'https://models.inference.ai.azure.com/'
-});
