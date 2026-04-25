acdde1c32d930f72751541fad4af78332e619e21
coding911 <coding0911@gmail.com>
2026-04-26 06:54:52 +0700

    fix ui

--- gamification-social-app/frontend/app/page.tsx
+++ gamification-social-app/frontend/app/page.tsx
@@ -28,9 +28,9 @@ export default function HomePage() {
             id="btn-login"
             // className="rounded-xl border border-white/30 bg-white/10 px-6 py-2.5 text-lg font-semibold text-white backdrop-blur-md transition-all duration-300 hover:bg-white/25 hover:shadow-lg hover:shadow-white/10"
             className="rounded-xl border border-cyan-300/40 bg-white/10 px-6 py-2.5 text-lg font-semibold text-white backdrop-blur-md
-              transition-all duration-200 ease-out
-              hover:bg-cyan-300/10 hover:border-cyan-300/70
-              hover:shadow-[0_0_10px_rgba(0,240,255,0.25)]"
+            transition-all duration-200 ease-out
+            hover:bg-cyan-300/10 hover:border-cyan-300/70
+            hover:shadow-[0_0_10px_rgba(0,240,255,0.25)]"
           >
             Đăng nhập
           </Link>
@@ -39,13 +39,13 @@ export default function HomePage() {
             id="btn-register"
             // className="rounded-xl bg-gradient-to-r from-violet-500 to-indigo-500 px-8 py-2.5 text-lg font-semibold text-white shadow-lg shadow-violet-500/30 transition-all duration-300 hover:from-violet-400 hover:to-indigo-400 hover:shadow-xl hover:shadow-violet-500/40"
             className="rounded-xl
-              bg-gradient-to-r from-[#2eea12] to-[#00e6ff]
-              px-8 py-2.5 text-lg font-semibold text-black
-              shadow-[inset_0_1px_0_rgba(255,255,255,0.4),0_0_10px_rgba(0,240,255,0.3),0_0_30px_rgba(0,240,255,0.2)]
-              transition-all duration-200 ease-out
-              hover:brightness-110
-              hover:shadow-[0_0_14px_rgba(0,240,255,0.35),0_0_40px_rgba(0,240,255,0.25)]
-              active:scale-95"
+            bg-gradient-to-r from-[#2eea12] to-[#00e6ff]
+            px-8 py-2.5 text-lg font-semibold text-black
+            shadow-[inset_0_1px_0_rgba(255,255,255,0.4),0_0_10px_rgba(0,240,255,0.3),0_0_30px_rgba(0,240,255,0.2)]
+            transition-all duration-200 ease-out
+            hover:brightness-110
+            hover:shadow-[0_0_14px_rgba(0,240,255,0.35),0_0_40px_rgba(0,240,255,0.25)]
+            active:scale-95"
           >
             Đăng ký
           </Link>
@@ -61,7 +61,11 @@ export default function HomePage() {
               <span>Bạn muốn:</span>
               <Typewriter />
             </p>
-            <p className="text-3xl font-semibold text-cyan-300 drop-shadow md:text-4xl">
+            {/* <p className="text-3xl font-semibold text-cyan-300 drop-shadow md:text-4xl"> */}
+            <p className="text-3xl font-semibold md:text-4xl
+            bg-gradient-to-r from-[#2eea12] to-[#00e6ff]
+            bg-clip-text text-transparent
+            drop-shadow-[0_0_10px_rgba(0,240,255,0.3)]">
               👉🏻 Web này dành cho bạn
             </p>
           </div>
